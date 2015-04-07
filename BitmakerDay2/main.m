49c212bd8ab34d27e515287c810da1497ccc5e64
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-07 10:30:14 -0400

    Initial Commit

---
+++ BitmakerDay2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BitmakerDay2
+//
+//  Created by Daniel Goncalves on 2015-04-07.
+//  Copyright (c) 2015 Daniel Goncalves. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
